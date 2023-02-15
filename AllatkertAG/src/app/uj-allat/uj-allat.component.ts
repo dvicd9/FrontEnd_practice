42c5fa5be2ce5ef8b02165acecb936657baf4324
Gregor <dvicd9@inf.elte.hu>
2023-02-15 22:12:49 +0100

    Utolso lepesek

--- AllatkertAG/src/app/uj-allat/uj-allat.component.ts
+++ AllatkertAG/src/app/uj-allat/uj-allat.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-uj-allat',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./uj-allat.component.scss']
 })
 export class UjAllatComponent implements OnInit {
+  gondozok: any[] = [];
+  ujAllat = {
+    nev: '',
+    faj: '',
+    erkezes: new Date().toISOString().substring(0, 10),
+    helye: '',
+    gondozo: ''
+  };
+  hibauzenet: string = '';
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+    this.http.get<any>('https://angular-vizsga-gyak-default-rtdb.europe-west1.firebasedatabase.app/gondozok.json')
+    .pipe(
+      catchError( (error: any) => {
+        this.hibauzenet = error.message;
+        return throwError(error);
+      })
+    )
+    .subscribe( (gondozok: any[]) => {
+      this.gondozok = gondozok;
+    });
+  }
+
+  felvesz(){
+    this.http.post<any>('https://angular-vizsga-gyak-default-rtdb.europe-west1.firebasedatabase.app/allatok.json', this.ujAllat)
+    .pipe(
+      catchError( (error: any) => {
+        this.hibauzenet = error.message;
+        return throwError(error);
+      })
+    )
+    .subscribe( () => {
+      this.router.navigate(['allataink']);
+    });
   }
 
 }
