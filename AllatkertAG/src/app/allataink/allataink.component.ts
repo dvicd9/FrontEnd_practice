39e5c8b891265911cf293285da261615b2bba231
Gregor <dvicd9@inf.elte.hu>
2023-02-15 21:48:39 +0100

    Allataink

--- AllatkertAG/src/app/allataink/allataink.component.ts
+++ AllatkertAG/src/app/allataink/allataink.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-allataink',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./allataink.component.scss']
 })
 export class AllatainkComponent implements OnInit {
+  allatainkOszlopok: string[] = ['nev', 'faj', 'gondozo', 'erkezes', 'helye', 'erkezes'];
+  allataink: any[] = [];
+  hibauzenet: String = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any>('https://angular-vizsga-gyak-default-rtdb.europe-west1.firebasedatabase.app/allatok.json')
+    .pipe(
+      catchError( (error: any) => {
+        this.hibauzenet = error.message;
+        return throwError(error);
+      }),
+      map( (allatok: any[]) => {
+        let ujAllatok: any[] = [];
+        Object.entries(allatok).forEach( ([key, value]) =>{
+          ujAllatok.push(value);
+        });
+        return ujAllatok;
+      })
+    )
+    .subscribe( (allatok: any[]) => {
+      this.allataink = allatok;
+      console.log(this.allataink);      
+    });
   }
 
 }
--- AllatkertAG/src/app/app.module.ts
+++ AllatkertAG/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NyitooldalComponent } from './nyitooldal/nyitooldal.component';
 import { AllatainkComponent } from './allataink/allataink.component';
 import { UjAllatComponent } from './uj-allat/uj-allat.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { UjAllatComponent } from './uj-allat/uj-allat.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatTableModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
